73d457f9cbe49374d409064a43d62247406fab85
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-18 23:31:37 +0200

    ex2.js done

--- ex2.js
+++ ex2.js
@@ -3,15 +3,22 @@ function boton1() {
 }
 
 function boton2() {
-  document.querySelector("#panel").style.backgroundColor = "rgb(255, 0, 0)";
+  document.querySelector("#panel").style.backgroundColor = "rgb(0, 0, 255)";
+document.getElementById('panel').backgroundColor.style.backgroundColor = 0, 0, 255
 }
-
 function asociamePorFavor() {
   document.querySelector("#panel").textContent =
     "Gracias por asociar la función al botón 3!";
 }
+document.getElementById("boton3").addEventListener("click", asociamePorFavor);
 
 function calcularLetrasTitulo() {
   // 1. Accede a la propiedad textContent del nodo h1. Guarda esa información en una variable
   // 2. Usa el método adecuado para obtener el número de carácteres del string en textContent. Pon ese valor en el textContent del #panel
+
+//const h1Element = document.querySelector('h1');
+//const numeroCaracteres = h1Element.textContent.length;
+//document.getElementById('panel').text,content =  numeroCaracteres
+
+document.getElementById('panel').textContent = document.querySelector('h1').textContent.length;
 }
